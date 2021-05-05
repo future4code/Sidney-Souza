bf30dbf9d66a1b9a8a09bff4a61c8cff1da266ec
sidney-souza <83144292+sidney-souza@users.noreply.github.com>
2021-05-05 16:55:35 -0300

    Exercício aula 5

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,56 @@
+// Exercício 1
+/*
+A. undefined
+B. b , null
+C. c , 20
+D. d , 3
+E. e , [3,19,5,6,7,8,9,10,11,12,13]
+F. f , 9
+
+*/ 
+
+// Exercício 2
+
+// SUBI NUM ÔNIBUS EM MIRROCOS 
+
+//26
+
+
+// Exercício de escrita de código
+
+//Exercício 1
+
+/*const nomeDoUsuario = prompt("Digite seu nome!")
+const emailDousuario = prompt("Digite se e-mail!")
+
+alert("O e-mail " + emailDousuario + " foi cadastrado com sucesso. " + "Seja bem-vindo " + nomeDoUsuario)
+*/
+
+// Exercício 2
+
+/*const array = [ "churrasco ","japonesa ","lasanha ","pudim ","salada"]
+
+alert(array)
+
+alert("Essas são minhas comidas preferidas" + array[0] +array[1])*/
+
+
+
+//Exercicio 3
+
+const listaDeTarefas =[] 
+listaDeTarefas[0] = prompt("Digite tarefa 1")
+listaDeTarefas[1] = prompt("Digite tarefa 2")
+listaDeTarefas[2] = prompt("digite tarefa 3")
+
+alert (listaDeTarefas)
+
+
+const indice =Number( prompt("Digite o índice de uma tarefa que já realizou: 0, 1 ou 2 "))
+
+
+
+
+
+
+
---
+++ semana1/variaveis/semana1/operadores/index.js
@@ -0,0 +1,55 @@
+/*1A. FALSE
+B.FALSE
+C.FALSE
+D.
+
+2.o comando prompt retorna uma string por tanto não efetara a soma.
+
+3.Chamar a função Number na frente do prompt.
+
+
+*/
+// Exercício 1
+/*const minhaIdade = Number( prompt("Digite sua idade!"))
+const idadeAmigo =Number( prompt("Digite a idade do seu melhor amigo!"))
+const resposta = minhaIdade > idadeAmigo
+const difIdade= minhaIdade -idadeAmigo
+
+console.log("Sua idadede é maior que a do seu melhor amigo? " ,resposta)
+console.log("Diferença de idade ",difIdade)*/
+
+
+// Exercício 2
+
+/*const numeroPar = Number( prompt ("Digite um numero par"))
+const divisor = 2
+const resultado = numeroPar % divisor
+console.log("resto da divisão ",resultado )
+
+// C. Se for inserido um numero par o resto sempre será 0
+// D. Retornará um numero diferente de 0*/
+
+//Exercício 3
+
+/*const idadeUsuário = Number(prompt("Digite sua idade"))
+
+console.log("Idade em meses " ,idadeUsuário* 12) 
+console.log("Idade em dias! " ,idadeUsuário * 365)
+console.log("Idade em Horas!" , idadeUsuário * 8760)*/
+
+
+// Exercício 4
+
+/*const primeiroNumero =Number(prompt("Digite um nímero" ))
+const segundoNumero = Number(prompt("Digite outro número"))
+
+console.log("O primeiro número é maior que o segundo?" , primeiroNumero > segundoNumero)
+console.log("O primeiro número é igual o segundo?" ,primeiroNumero == segundoNumero)
+console.log("O primeiro número é divisível pelo segundo?")
+console.log("O segundo numero é divisível primeiro")*/
+
+
+const nome = "chijo"
+const nomeAlterado =nome.toLowerCase()
+console.log(nomeAlterado)
+
