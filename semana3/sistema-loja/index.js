cc9382c1fbbf51e06c41925ff04adec83ef20bd0
sidney-souza <83144292+sidney-souza@users.noreply.github.com>
2021-05-20 13:34:35 -0300

    lojinha

---
+++ semana3/sistema-loja/index.js
@@ -0,0 +1,10 @@
+```function calculaPrecoTotal(quantidade) {
+    // Escreva seu código aqui
+    let precaFinal  = 1.30 * quantidade
+    let precaFinal2 = 1.00 * quantidade
+    if(quantidade< 12){
+      return precaFinal
+    }else{
+      return precaFinal2
+    }
+  }```
