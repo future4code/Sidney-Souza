8013c14713a0dd9ec4685c06bcac048adc8884c0
sidney-souza <83144292+sidney-souza@users.noreply.github.com>
2021-08-09 17:11:56 -0300

    exercicios

--- template-node-npm/src/ExerciciosAula41.js
+++ template-node-npm/src/ExerciciosAula41.js
@@ -1,18 +1,18 @@
-/*1 A- process.argv []
-1 B
+//1 A- process.argv []
+//1 B
 const nome = "Sidney"
 const idade = 35
 
 console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
 
-1 C
+//1 C
 const nome = "Sidney" 
 const idade = Number(process.argv[2])
 const idadeNova = Number(process.argv[3])
 
 console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade+idadeNova}`)
 
-2
+//2
 const operacao=process.argv [2]
 const numero1=process.argv [3]
 const numero2=process.argv [4]
@@ -34,9 +34,9 @@ switch(operacao){
         console.log("Digite a operação desejada")   
                 
 }
-*/
 
-3
+
+//3
 const listaDETarefas = process.argv [2]
 const novaTarefa = process.argv [3]
 
